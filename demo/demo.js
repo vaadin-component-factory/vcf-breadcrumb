efedca4ea6b360de219ec280ae9f0971a379da40
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-10-28 10:41:14 +0000

    cleanup

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,3 @@
 import '@polymer/iron-demo-helpers/demo-pages-shared-styles';
 import '@polymer/iron-demo-helpers/demo-snippet';
-import '@vaadin/vaadin-lumo-styles/icons.js';
 import '../dist/src/vcf-breadcrumbs.js';
